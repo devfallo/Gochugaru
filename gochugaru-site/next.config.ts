9e6d7429c011378f0f40fd39ecee89ff928c22b9
DJJeong <sayjdj@gmail.com>
2025-07-27 22:38:30 +0900

    Feat: Configure GitHub Pages for project page hosting

--- gochugaru-site/next.config.ts
+++ gochugaru-site/next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/Gochugaru',
+  assetPrefix: '/Gochugaru/',
   /* config options here */
 };
 
